a368aebaca2ff19ff9c10cf7f659a9c6b37e8c98
Jason <yeon971105@icloud.com>
2023-01-30 21:21:13 -0800

    first commit

---
+++ 1.py
@@ -0,0 +1,23 @@
+from math import *
+
+
+character_name = "Giraffe Academy"
+character_age = -50.34324125
+is_male = True
+print('Hello\nworld', character_name, character_age, is_male)
+print(character_name.upper().isupper())
+print(character_name[0])
+print(character_name.index("Acad"))
+print(character_name.replace("Acad", "Elephant"))
+print(str(character_age) + " my favorite numnber")
+print(abs(character_age))
+print(pow(2, 3))
+print(character_age ** 2)
+print(max(character_age, 10))
+print(min(character_age, 10))
+print(round(character_age))
+print(floor(character_age))
+print(9/2)
+nam = input('Who are you?')
+#print(nam)
+
